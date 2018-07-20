afb5bb3d40171b7e6a2b8c0271ac1712d10463fe
yaoxh6 <2879813862@qq.com>
2018-07-20 23:07:01 +0800

    加了chessAttack，目前用了Tag方法得到GameObject

--- BingMaYong/Assets/Scripts/Chess.cs
+++ BingMaYong/Assets/Scripts/Chess.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Chess : MonoBehaviour {
 
+    GameObject player;
+    GameObject enemy;
 	// Use this for initialization
 	void Start () {
-		
+        player = GameObject.FindGameObjectWithTag("Chess");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Attack(player,enemy);
 	}
+
+    void Attack(GameObject player,GameObject enemy)
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            player.GetComponent<Animator>().SetTrigger("IsChessAttack");
+        }
+    }
 }
